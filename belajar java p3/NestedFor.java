7376a7780188829897068726466e3c7ca2e82b28
dwikifahrezi <dwikifahrezi17@gmail.com>
2024-04-04 17:17:02 +0700

    latihan java

---
+++ belajar java p3/NestedFor.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PernyataanFor;
+
+/**
+ *
+ * @author DELL
+ */
+public class NestedFor {
+    public static void main(String[] args)
+{
+int a, b;
+
+for(a = 1; a <= 5; a++)
+{
+System.out.println();
+for(b = a; b <= 5; b++)
+{
+System.out.println(a);
+}   
+}
+System.out.println("\n");
+}
+}
